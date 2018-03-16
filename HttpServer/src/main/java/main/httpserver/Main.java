06526a627dc344ce4c6886864f6f5b303b48f937
d.moreno <daniel.moreno.d@mail.escuelaing.edu.co>
2018-03-15 22:20:51 -0500

    arem

---
+++ HttpServer/src/main/java/main/httpserver/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.httpserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ *
+ * @author daniel
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(new Integer(System.getenv("PORT")));
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        while (true){
+            executor.execute(new HttpServer(serverSocket.accept()));           
+        }
+    }  
+}
